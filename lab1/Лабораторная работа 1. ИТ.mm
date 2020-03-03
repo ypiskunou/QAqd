c196ef758a1793122b6f67d8a383087286bf2616
Yury Piskunou <ypiskunou@gmail.com>
2020-03-03 03:05:52 +0300

    lab1it

---
+++ lab1/Лабораторная работа 1. ИТ.mm
@@ -0,0 +1,25 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1583151716053" ID="ID_1312722715" MODIFIED="1583193753687" TEXT="&#x41b;&#x430;&#x431;&#x43e;&#x440;&#x430;&#x442;&#x43e;&#x440;&#x43d;&#x430;&#x44f; &#x440;&#x430;&#x431;&#x43e;&#x442;&#x430; 1. &#x418;&#x422;">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1583152122372" ID="ID_1676500463" MODIFIED="1583193774556" POSITION="right" TEXT="&#x414;&#x435;&#x43a;&#x43e;&#x43c;&#x43f;&#x43e;&#x437;&#x438;&#x446;&#x438;&#x44f; &#x444;&#x43e;&#x440;&#x43c;&#x44b; &#x440;&#x435;&#x433;&#x438;&#x441;&#x442;&#x440;&#x430;&#x446;&#x438;&#x438;">
+<edge COLOR="#ff0000" STYLE="sharp_bezier" WIDTH="2"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1583152307338" ID="ID_928976094" MODIFIED="1583152401229" TEXT="&#x41f;&#x43e;&#x43b;&#x435; &#x438;&#x43c;&#x435;&#x43d;&#x438;"/>
+<node CREATED="1583152412813" ID="ID_297891188" MODIFIED="1583152422938" TEXT="&#x41f;&#x43e;&#x43b;&#x435; &#x438;&#x43c;&#x44d;&#x439;&#x43b;&#x430;"/>
+<node CREATED="1583152442206" ID="ID_183980583" MODIFIED="1583152457531" TEXT="&#x41f;&#x43e;&#x43b;&#x435; &#x43f;&#x430;&#x440;&#x43e;&#x43b;&#x44f;"/>
+<node CREATED="1583152460657" ID="ID_1063773788" MODIFIED="1583152474069" TEXT="&#x41f;&#x43e;&#x43b;&#x435; &#x43f;&#x43e;&#x434;&#x442;&#x432;&#x435;&#x436;&#x434;&#x435;&#x43d;&#x438;&#x44f; &#x43f;&#x430;&#x440;&#x43e;&#x43b;&#x44f;"/>
+<node CREATED="1583152496516" ID="ID_951168765" MODIFIED="1583152519248" TEXT="&#x41a;&#x43d;&#x43e;&#x43f;&#x43a;&#x430; &quot;&#x41e;&#x442;&#x43f;&#x440;&#x430;&#x432;&#x438;&#x442;&#x44c;&quot;"/>
+</node>
+<node CREATED="1583152138908" ID="ID_820817913" MODIFIED="1583193768938" POSITION="left" TEXT="&#x422;&#x435;&#x441;&#x442; &#x434;&#x438;&#x437;&#x430;&#x439;&#x43d;. Use cases">
+<edge COLOR="#0000ff" STYLE="sharp_bezier" WIDTH="2"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1583152541752" ID="ID_1108329425" MODIFIED="1583152576767" TEXT="&#x41f;&#x43e;&#x43b;&#x435; &#x438;&#x43c;&#x435;&#x43d;&#x438; &#x43f;&#x443;&#x441;&#x442;&#x43e;"/>
+<node CREATED="1583152582489" ID="ID_1699591970" MODIFIED="1583152929106" TEXT="&#x41d;&#x435;&#x432;&#x435;&#x440;&#x43d;&#x44b;&#x439; &#x438;&#x43c;&#x435;&#x439;&#x43b;"/>
+<node CREATED="1583152956064" ID="ID_857296893" MODIFIED="1583192736234" TEXT="&#x412;&#x441;&#x435; &#x43f;&#x43e;&#x43b;&#x44f; &#x437;&#x430;&#x43f;&#x43e;&#x43b;&#x43d;&#x435;&#x43d;&#x44b; &#x432;&#x435;&#x440;&#x43d;&#x43e;, &#x43d;&#x43e;...">
+<node CREATED="1583173912268" ID="ID_1435162880" MODIFIED="1583173920378" TEXT=" &#x43f;&#x43e;&#x43b;&#x44c;&#x437;&#x43e;&#x432;&#x430;&#x442;&#x435;&#x43b;&#x44c; &#x441; &#x442;&#x430;&#x43a;&#x438;&#x43c; &#x438;&#x43c;&#x435;&#x43d;&#x435;&#x43c; &#x443;&#x436;&#x435; &#x435;&#x441;&#x442;&#x44c;"/>
+<node CREATED="1583192799216" ID="ID_94877950" MODIFIED="1583192823959" TEXT="&#x412;&#x43c;&#x435;&#x441;&#x442;&#x43e; &#x43a;&#x43b;&#x438;&#x43a;&#x430; &#x43f;&#x43e; &#x43a;&#x43d;&#x43e;&#x43f;&#x43a;&#x435; &#x43d;&#x430;&#x436;&#x438;&#x43c;&#x430;&#x435;&#x442;&#x441;&#x44f; Enter"/>
+</node>
+</node>
+</node>
+</map>
